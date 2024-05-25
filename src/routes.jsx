d1efdab3886eb7b4e19377289255eff81f8c4870
Pedro Daniel <pedrinhodan.08@gmail.com>
2024-05-25 19:49:03 -0300

    route tests 2

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,27 +4,20 @@ import { About } from "./pages/About/AboutIndex"
 import { Contact } from "./pages/Contacts/contactIndex"
 import { Projects } from "./pages/Projects/projectsIndex"
 import { NotFound } from "./pages/NotFound/notFoundIndex"
-import { Header } from "./Components/Header/headerIndex"
-import { Container } from "./Components/Container/containerIndex"
-import { Footer } from "./Components/Footer/footerIndex"
-// import { PageBase } from "./pages/PageBase/pageBaseIndex"
+import { PageBase } from "./pages/PageBase/pageBaseIndex"
 
 export function AppRoutes() {
     return (
         <BrowserRouter>
-            <Header />
-            <Container>
-                <Routes>
-                    {/* <Route path="/" exact element={<PageBase />}> */}
-                    <Route path="/" exact element={<Home />} />
+            <Routes>
+                <Route path="/" element={<PageBase />}>
+                    <Route index exact element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/contacts" element={<Contact />} />
                     <Route path="*" element={<NotFound />} />
-                    {/* </Route> */}
-                </Routes>
-            </Container>
-            <Footer />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
