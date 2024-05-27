7bdc36122e1d2c1e2f27af67835c96ed9978144c
Pedro Daniel <pedrinhodan.08@gmail.com>
2024-05-27 15:58:49 -0300

    icons change

--- src/Components/Icons/iconsIndex.jsx
+++ src/Components/Icons/iconsIndex.jsx
@@ -4,16 +4,18 @@ import { IoLogoCss3 } from "react-icons/io";
 import { IoLogoReact } from "react-icons/io5";
 
 export function Icons({ lang }) {
-    if (lang.toLowerCase() === "javascript") {
-        return <span><IoLogoJavascript /></span>
-    }
-    if (lang.toLowerCase() === "html") {
-        return <span><IoLogoHtml5 /></span>
-    }
-    if (lang.toLowerCase() === "css") {
-        return <span><IoLogoCss3 /></span>
-    }
-    if (lang.toLowerCase() === "react") {
-        return <span><IoLogoReact /></span>
+    switch (lang.toLowerCase()) {
+        case "javascript":
+            return <span><IoLogoJavascript /></span>
+            break
+        case "html":
+            return <span><IoLogoHtml5 /></span>
+            break
+        case "css":
+            return <span><IoLogoCss3 /></span>
+            break
+        case "react":
+            return <span><IoLogoReact /></span>
+            break
     }
 }
