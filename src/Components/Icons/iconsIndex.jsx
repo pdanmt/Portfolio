95c680a9fc8ddebd6e5dfb12fc110d986af84200
Pedro Daniel <pedrinhodan.08@gmail.com>
2024-06-12 14:59:53 -0300

    icons ts commit

--- src/Components/Icons/iconsIndex.jsx
+++ src/Components/Icons/iconsIndex.jsx
@@ -2,11 +2,15 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { IoLogoHtml5 } from "react-icons/io";
 import { IoLogoCss3 } from "react-icons/io";
 import { IoLogoReact } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
 
 export function Icons({ lang }) {
     switch (lang.toLowerCase()) {
         case "javascript":
             return <span><IoLogoJavascript /></span>
+            break
+        case "typescript":
+            return <span><SiTypescript /></span>
             break
         case "html":
             return <span><IoLogoHtml5 /></span>
