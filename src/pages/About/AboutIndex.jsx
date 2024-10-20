597948d7396e1ea94f5a01ab0a3b1bd2463e7ca1
Pedro Daniel <pedrinhodan.08@gmail.com>
2024-10-20 15:58:16 -0300

    ts logo commit

--- src/pages/About/AboutIndex.jsx
+++ src/pages/About/AboutIndex.jsx
@@ -5,6 +5,7 @@ import { IoLogoJavascript } from "react-icons/io5";
 import { IoLogoHtml5 } from "react-icons/io";
 import { IoLogoCss3 } from "react-icons/io";
 import { IoLogoReact } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
 
 export function About() {
     return (
@@ -14,18 +15,21 @@ export function About() {
                 <section className={styles.sectionSobre}>
                     <h2>Sobre</h2>
                     <p><strong>Sou <span>Pedro Daniel</span></strong></p>
-                    <p className={styles.pBlue}>Dev Front End</p>
-                    <p>Estudo desenvolvimento Web desde 2020.</p>
+                    <p className={styles.pBlue}>Desenvolvedor Front End</p>
+                    <p>Estudo desenvolvimento Web desde 2024</p>
                     <p>Sou apaixonado em transformar ideias em realidade digital</p>
-                    <p>Estudante de desenvolvimento Web Front-End, focado em aplicações dinâmicas e intuitivas, com foco na experiência do usuário.</p>
+                    <p>Estudante de desenvolvimento Web Front-End, com foco em criação de aplicações úteis para resolução de problemas diversos.</p>
                 </section>
             </div>
             <section className={styles.sectionTechs}>
                 <h2>Techs</h2>
-                <span><IoLogoJavascript /></span>
-                <span><IoLogoHtml5 /></span>
-                <span><IoLogoCss3 /></span>
-                <span><IoLogoReact /></span>
+                <div>
+                    <span><IoLogoJavascript /></span>
+                    <span><SiTypescript size='3.65rem' /></span>
+                    <span><IoLogoHtml5 /></span>
+                    <span><IoLogoCss3 /></span>
+                    <span><IoLogoReact /></span>
+                </div>
             </section>
         </main>
     )
